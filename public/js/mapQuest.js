3c41e6dd1e24af81c18e82f03cca1c1b71a69260
Keith Lim <k2lim@ucsd.edu>
2021-02-25 10:47:26 -0800

    up to map function

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'E8qXCXn9hebau8ZMaR1F1JAj7vc7aAEj';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+  	center: [32.88, -117.236],
+  	layers: L.mapquest.tileLayer('map'),
+  	zoom: 12
+	});
+
+	// Add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
